224ffb69da5a3044cc610e5fbdc5ab1daaf1a733
cleitonme <cleitonme@gmail.com>
2024-07-09 14:31:06 -0300

    mensagem deletada e editada

--- backend/src/services/WbotServices/wbotMessageListener.ts
+++ backend/src/services/WbotServices/wbotMessageListener.ts
@@ -3,15 +3,15 @@ import { Client } from "whatsapp-web.js";
 import HandleMessage from "./helpers/HandleMessage";
 import HandleMsgAck from "./helpers/HandleMsgAck";
 import VerifyCall from "./VerifyCall";
+import handleMsgEdit from "./helpers/handleMsgEdit";
+import verifyRevoked from "./helpers/verifyRevoked";
 
 interface Session extends Client {
   id: number;
 }
 
 const wbotMessageListener = (wbot: Session): void => {
-  // const queue = `whatsapp::${wbot.id}`;
   wbot.on("message_create", async msg => {
-    // desconsiderar atualização de status
     if (msg.isStatus) {
       return;
     }
@@ -24,6 +24,17 @@ const wbotMessageListener = (wbot: Session): void => {
 
   wbot.on("message_ack", async (msg, ack) => {
     HandleMsgAck(msg, ack);
+  });
+  
+  wbot.on("message_edit", async (msg, newBody, oldBody) => {
+    handleMsgEdit(msg, newBody as string);
+  });
+  
+  wbot.on("message_revoke_everyone", async (after, before) => {
+    const msgBody: string | undefined = before?.body;
+    if (msgBody !== undefined) {
+      verifyRevoked(msgBody || "");
+    }
   });
 
   wbot.on("call", async call => {
