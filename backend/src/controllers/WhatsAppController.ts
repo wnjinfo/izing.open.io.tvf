819057d8a98cb9631ef83b6c40ddb5531caf7ab4
cleitonme <cleitonme@gmail.com>
2024-07-09 13:12:09 -0300

    limite conexoes funcional

--- backend/src/controllers/WhatsAppController.ts
+++ backend/src/controllers/WhatsAppController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { getIO } from "../libs/socket";
 import { removeWbot } from "../libs/wbot";
 import AppError from "../errors/AppError";
+import Tenant from "../models/Tenant"; 
 
 import DeleteWhatsAppService from "../services/WhatsappService/DeleteWhatsAppService";
 import ListWhatsAppsService from "../services/WhatsappService/ListWhatsAppsService";
@@ -26,12 +27,29 @@ export const show = async (req: Request, res: Response): Promise<Response> => {
   return res.status(200).json(whatsapp);
 };
 
+const getTenantmaxConnections = async (tenantId: string): Promise<number> => {
+  try {
+    const tenant = await Tenant.findOne({ where: { id: tenantId } });
+    if (!tenant) {
+      throw new AppError("Tenant not found", 404);
+    }
+    return tenant.maxConnections;
+  } catch (error) {
+    throw new AppError("Error fetching maxTenants", 500);
+  }
+};
+
 export const store = async (req: Request, res: Response): Promise<Response> => {
   const { whatsappId } = req.params;
   const whatsappData = req.body;
   const { tenantId } = req.user;
   const whatsapps = await ListWhatsAppsService(tenantId);
   if (whatsapps.length >= Number(process.env.CONNECTIONS_LIMIT)) {
+    throw new AppError("ERR_NO_PERMISSION_CONNECTIONS_LIMIT", 400);
+  }
+  
+  const maxConnections = await getTenantmaxConnections(String(tenantId));
+  if (whatsapps.length >= maxConnections) {
     throw new AppError("ERR_NO_PERMISSION_CONNECTIONS_LIMIT", 400);
   }
 
